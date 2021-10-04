47dd0dd89c48e8a64940f69ff3c632c17e618bd3
papan.sarkar <papan.sarkar@preflet.com>
2021-10-04 21:18:40 +0530

    updated settings file

--- alvaro/settings.py
+++ alvaro/settings.py
@@ -120,7 +120,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
-STATIC_ROOT = '/var/www/rycc/static'
+STATIC_ROOT = '/var/www/papansarkar/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
